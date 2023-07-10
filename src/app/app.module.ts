82af2021f61aeab19a32026b1413539562fa0f32
Mquinteagu <marianaqagudelo@gmail.com>
2023-07-10 16:33:11 -0500

    Desafio6_Nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { LayoutModule } from './layout/layout.module';
 import { RoutesModule } from './routes/routes.module';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { RoutesModule } from './routes/routes.module';
     AppRoutingModule,
     SharedModule,
     LayoutModule,
-    RoutesModule
+    RoutesModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes/components/ingresar/ingresar.component.ts
+++ src/app/routes/components/ingresar/ingresar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-ingresar',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class IngresarComponent {
 
+  constructor(
+    private _authService: AuthService
+  ){}
+
+  loginGoogle(){
+    this._authService.loginWithGoogle().then((response) =>{
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
 }

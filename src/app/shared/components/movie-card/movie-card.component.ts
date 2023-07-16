f0e690a9d17bd973d2e35376d0f1ae1807f2af3b
Mquinteagu <marianaqagudelo@gmail.com>
2023-07-15 23:29:52 -0500

    Desafio-7

--- src/app/shared/components/movie-card/movie-card.component.ts
+++ src/app/shared/components/movie-card/movie-card.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MoviesSeries } from 'src/app/types/moviesSeries';
 import { FirestoreService } from 'src/app/services/firestore.service';
-import { Serie } from 'src/app/types/Series';
-import { Movie } from 'src/app/types/Movies';
 
 @Component({
   selector: 'app-movie-card',
@@ -16,8 +14,6 @@ export class MovieCardComponent {
   @Input() id: number = 0;
   @Input() showButtons: boolean = false;
   @Input() estadoBoton: boolean = true;
-  @Input() data: Serie [] | undefined;
-  @Input() filter: string | undefined;
 
   @Output() AddMovie = new EventEmitter<number>();
   @Output() DeleteMovie = new EventEmitter<number>();
